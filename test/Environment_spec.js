4cd50b59c6da6433f03bf8d4755febb1d8a3a97e
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-09-16 12:42:23 -0400

    Set and restore environment variables.

--- test/Environment_spec.js
+++ test/Environment_spec.js
@@ -25,6 +25,41 @@ describe("an Environment", function () {
 		return set.bind(null, name, previous);
 	}
 
+	function describeRestoreFunction (context) {
+		describe("and invoking the restore function", function () {
+			var restore;
+
+			before(function (done) {
+				restore = context.result();
+				done();
+			});
+
+			after(function (done) {
+				restore();
+				done();
+			});
+
+			it("undoes the set operation", function (done) {
+				expect(environment.get("foo"), "value").to.equal(context.originalValue);
+				done();
+			});
+		});
+	}
+
+	function describeSet (context) {
+		it("sets the value", function (done) {
+			expect(environment.get("foo"), "value").to.equal("bar");
+			done();
+		});
+
+		it("returns a 'restore' function", function (done) {
+			expect(context.result, "type").to.be.a("function");
+			done();
+		});
+
+		describeRestoreFunction(context);
+	}
+
 	before(function (done) {
 		environment = new Environment();
 		done();
@@ -68,6 +103,79 @@ describe("an Environment", function () {
 
 		it("returns the value", function (done) {
 			expect(result, "result").to.equal("bar");
+			done();
+		});
+	});
+
+	describe("setting a variable that is not defined", function () {
+		var context = {};
+		var restore;
+
+		before(function (done) {
+			restore = set("FOO");
+
+			context.originalValue = environment.get("foo");
+			context.result        = environment.set("foo", "bar");
+			done();
+		});
+
+		after(function (done) {
+			restore();
+			done();
+		});
+
+		describeSet(context);
+	});
+
+	describe("setting a variable that is already defined", function () {
+		var context = {};
+		var restore;
+
+		before(function (done) {
+			restore = set("FOO", "bar");
+
+			context.originalValue = environment.get("foo");
+			context.result        = environment.set("foo", "bar");
+			done();
+		});
+
+		after(function (done) {
+			restore();
+			done();
+		});
+
+		describeSet(context);
+	});
+
+	describe("restoring all changes", function () {
+		var restore = [];
+
+		var bar;
+		var foo;
+
+		before(function (done) {
+			bar = environment.get("bar");
+			foo = environment.get("foo");
+
+			restore.push(set("BAR"));
+			restore.push(set("FOO"));
+
+			environment.set("bar", "bar");
+			environment.set("foo", "foo");
+			environment.restore();
+			done();
+		});
+
+		after(function (done) {
+			restore.forEach(function (restore) {
+				return restore();
+			});
+			done();
+		});
+
+		it("reverts all changes to the environment", function (done) {
+			expect(environment.get("bar"), "bar").to.equal(bar);
+			expect(environment.get("foo"), "foo").to.equal(foo);
 			done();
 		});
 	});
